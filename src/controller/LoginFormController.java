ae0134968d82c37a9b6c36ae6b160e7b97d90dfe
Gihara Wicktramatunge <giharanw@gmail.com>
2022-01-29 11:34:17 +0530

    Add LoginForm

---
+++ src/controller/LoginFormController.java
@@ -0,0 +1,19 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginFormController {
+    public TextField txtUserName;
+    public PasswordField txtPassword;
+    public Button btnSignIn;
+
+    public void initialize(){
+
+    }
+
+    public void btnSignIn_OnAction(ActionEvent actionEvent) {
+    }
+}
--- src/controller/SplashScreenFormController.java
+++ src/controller/SplashScreenFormController.java
@@ -1,12 +1,40 @@
 package controller;
 
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
 
 public class SplashScreenFormController {
     public Label lblStatus;
 
     public void initialize(){
-
+        new Thread(() -> {
+            try {
+                Thread.sleep(1000);
+                Platform.runLater(() -> {
+                    try{
+                        AnchorPane root = FXMLLoader.load(getClass().getResource("/view/LoginForm.fxml"));
+                        Scene loginScene = new Scene(root);
+                        Stage stage = (Stage) lblStatus.getScene().getWindow();
+                        stage.setScene(loginScene);
+                        stage.sizeToScene();
+                        stage.centerOnScreen();
+                        stage.show();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
-
 }

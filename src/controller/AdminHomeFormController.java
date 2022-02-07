08d8903dfbc1563b787000e1c23028839e21c6c3
Gihara Wicktramatunge <giharanw@gmail.com>
2022-02-07 23:13:59 +0530

    Implement Home - Part 1

--- src/controller/AdminHomeFormController.java
+++ src/controller/AdminHomeFormController.java
@@ -1,10 +1,15 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import security.SecurityContextHolder;
 
+import java.io.IOException;
 import java.security.Principal;
 
 public class AdminHomeFormController {
@@ -21,7 +26,27 @@ public class AdminHomeFormController {
         lblAdmin.setText("Welcome "+ SecurityContextHolder.getPrincipal().getName()+"!");
     }
 
-    public void btnRecordAttendance_OnAction(ActionEvent actionEvent) {
+    public void btnRecordAttendance_OnAction(ActionEvent actionEvent) throws IOException {
+        Stage stage = new Stage();
+        AnchorPane root = FXMLLoader.load(getClass().getResource("/view/RecordAttendanceForm.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Student Attendance Marking System: Record Attendance");
+        stage.setResizable(false);
+        stage.initOwner(lblAdmin.getScene().getWindow());
+        stage.show();
+    }
+
+    public void btnSignOut_OnAction(ActionEvent actionEvent) throws IOException {
+        SecurityContextHolder.clear();
+        Stage stage = new Stage();
+        AnchorPane root = FXMLLoader.load(getClass().getResource("/view/LoginForm.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Student Attendance Marking System: Login");
+        stage.setResizable(false);
+        stage.show();
+        ((Stage) lblAdmin.getScene().getWindow()).close();
     }
 
     public void btnViewReports_OnAction(ActionEvent actionEvent) {
@@ -34,8 +59,5 @@ public class AdminHomeFormController {
     }
 
     public void btnBackupRestore_OnAction(ActionEvent actionEvent) {
-    }
-
-    public void btnSignOut_OnAction(ActionEvent actionEvent) {
     }
 }
--- src/controller/UserHomeFormController.java
+++ src/controller/UserHomeFormController.java
@@ -1,9 +1,15 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import security.SecurityContextHolder;
+
+import java.io.IOException;
 
 public class UserHomeFormController {
 
@@ -17,15 +23,32 @@ public class UserHomeFormController {
         lblUser.setText("Welcome "+ SecurityContextHolder.getPrincipal().getName()+"!");
     }
 
-    public void btnSignOut_OnAction(ActionEvent actionEvent) {
+    public void btnRecordAttendance_OnAction(ActionEvent actionEvent) throws IOException {
+        Stage stage = new Stage();
+        AnchorPane root = FXMLLoader.load(getClass().getResource("/view/RecordAttendanceForm.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Student Attendance Marking System: Record Attendance");
+        stage.setResizable(false);
+        stage.initOwner(lblUser.getScene().getWindow());
+        stage.show();
+    }
+
+    public void btnSignOut_OnAction(ActionEvent actionEvent) throws IOException {
+        SecurityContextHolder.clear();
+        Stage stage = new Stage();
+        AnchorPane root = FXMLLoader.load(getClass().getResource("/view/LoginForm.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Student Attendance Marking System: Login");
+        stage.setResizable(false);
+        stage.show();
+        ((Stage) lblUser.getScene().getWindow()).close();
     }
 
     public void btnUserProfile_OnAction(ActionEvent actionEvent) {
     }
 
     public void btnViewReports_OnAction(ActionEvent actionEvent) {
-    }
-
-    public void btnRecordAttendance_OnAction(ActionEvent actionEvent) {
     }
 }

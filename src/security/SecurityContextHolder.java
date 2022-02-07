16a40635eba7450fe10d444578b6bd567b95cebf
Gihara Wicktramatunge <giharanw@gmail.com>
2022-02-07 19:22:16 +0530

    Add Principal and SecurityContextHolder

---
+++ src/security/SecurityContextHolder.java
@@ -0,0 +1,14 @@
+package security;
+
+public class SecurityContextHolder {
+
+    private static Principal principal;
+
+    public static Principal getPrincipal() {
+        return principal;
+    }
+
+    public static void setPrincipal(Principal principal) {
+        SecurityContextHolder.principal = principal;
+    }
+}
